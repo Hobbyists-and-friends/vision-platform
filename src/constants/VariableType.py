06178a5bd6ecd673f161821d1f7861318af37178
threezinedine <threezinedine@gmail.com>
2023-04-02 04:45:18 +0700

    add some variable types

---
+++ src/constants/VariableType.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class VariableType(Enum):
+    NUMBER = 'number'
+    STRING = 'string'
+    BUTTON = 'button'
+    IMAGE = 'image'
+    OPERATION = 'operation'
+    OPERATION_FLOW = 'operation_flow'
---
+++ src/constants/__init__.py
@@ -0,0 +1,1 @@
+from .VariableType import VariableType
